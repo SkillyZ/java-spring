2bc65d70b0a792ffca35357e83fa70a140a7694e
zhouminqiang <1254109699@qq.com>
2018-01-07 16:49:53 +0800

    spring cloud eureka

---
+++ cloudclient/src/main/java/com/skilly/client/CloudclientApplication.java
@@ -0,0 +1,12 @@
+package com.skilly.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudclientApplication.class, args);
+	}
+}
