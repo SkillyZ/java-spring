914eda9f56f67c377577e67f7603ae11b8716f95
zhouminqiang <1254109699@qq.com>
2018-01-09 02:49:04 +0800

    server config

--- server-config/src/main/java/com/skilly/ServerConfigApplication.java
+++ server-config/src/main/java/com/skilly/ServerConfigApplication.java
@@ -3,6 +3,7 @@ package com.skilly;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableConfigServer
