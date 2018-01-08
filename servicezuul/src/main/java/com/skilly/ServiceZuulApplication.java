7dc24150190f4e9c7ec4207432408bb7b16a30cb
zhouminqiang <1254109699@qq.com>
2018-01-09 01:45:21 +0800

    add service zuul

---
+++ servicezuul/src/main/java/com/skilly/ServiceZuulApplication.java
@@ -0,0 +1,16 @@
+package com.skilly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@EnableEurekaClient
+@SpringBootApplication
+public class ServiceZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceZuulApplication.class, args);
+	}
+}
