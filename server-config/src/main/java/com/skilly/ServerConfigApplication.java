e540c3e56fc23158afa78db6914ab65930c5cb83
zhouminqiang <1254109699@qq.com>
2018-01-09 02:32:28 +0800

    server config

--- server-config/src/main/java/com/skilly/ServerConfigApplication.java
+++ server-config/src/main/java/com/skilly/ServerConfigApplication.java
@@ -2,11 +2,15 @@ package com.skilly;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class ServerConfigApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServerConfigApplication.class, args);
 	}
 }
+
+
