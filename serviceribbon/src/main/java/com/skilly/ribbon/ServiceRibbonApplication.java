9ea1392545f6fc058eb66900b8006880f089ffea
zhouminqiang <1254109699@qq.com>
2018-01-09 01:10:15 +0800

    Hystrix Dashboard

--- serviceribbon/src/main/java/com/skilly/ribbon/ServiceRibbonApplication.java
+++ serviceribbon/src/main/java/com/skilly/ribbon/ServiceRibbonApplication.java
@@ -5,12 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableHystrix
+@EnableHystrixDashboard
 public class ServiceRibbonApplication {
 
 	//在工程的启动类中,通过@EnableDiscoveryClient向服务中心注册；并且向程序的ioc注入一个bean: restTemplate;并通过@LoadBalanced注解表明这个restRemplate开启负载均衡的功能。
