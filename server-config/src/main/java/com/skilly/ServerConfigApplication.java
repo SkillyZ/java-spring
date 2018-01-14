99e50468ddc0f403d6211c715a45bab9e79c5a2b
zhouminqiang <1254109699@qq.com>
2018-01-14 21:27:07 +0800

    eureka-server 高可用的分布式配置中心(Spring Cloud Config)

--- server-config/src/main/java/com/skilly/ServerConfigApplication.java
+++ server-config/src/main/java/com/skilly/ServerConfigApplication.java
@@ -3,7 +3,6 @@ package com.skilly;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableConfigServer
