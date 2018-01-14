dc08d5b12351359b7ee9bdb5a45b29b54ca5a9e8
zhouminqiang <1254109699@qq.com>
2018-01-14 21:47:53 +0800

    eureka-server 高可用的分布式配置中心(Spring Cloud Config)

--- server-config/src/main/java/com/skilly/ServerConfigApplication.java
+++ server-config/src/main/java/com/skilly/ServerConfigApplication.java
@@ -2,15 +2,17 @@ package com.skilly;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableDiscoveryClient
 public class ServerConfigApplication {
 
+//	启动类添加@EnableDiscoveryClient激活对配置中心的支持
 	public static void main(String[] args) {
 		SpringApplication.run(ServerConfigApplication.class, args);
 	}
 }
-
 
