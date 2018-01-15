dd862232abd546cfc3525c455dc8f77ff1cf4067
周闽强 <zhouminqiang@kuainiugroup.com>
2018-01-15 16:55:29 +0800

    schedule timer

--- java-api/src/main/java/com/skilly/timer/MyTimer.java
+++ java-api/src/main/java/com/skilly/timer/MyTimer.java
@@ -3,6 +3,8 @@ package com.skilly.timer;
 /**
  * Created by ${1254109699@qq.com} on 2018/1/15.
  */
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Timer;
 
 /**
@@ -21,6 +23,73 @@ public class MyTimer {
 
         // 通过 Timer 定时定频率调用 MyTimerTask 的业务逻辑
         // 即第一次执行是在当前时间的两秒之后，之后每隔一秒钟执行一次
-        timer.schedule(myTimerTask,2000L,1000L);
+        //timer.schedule(myTimerTask,2000L,1000L);
+
+        /**
+         * 获取当前时间，并设置成距离当前时间三秒之后的时间
+         * 如当前时间是2016-11-10 23:59:57
+         * 则设置后的时间则为2016-11-11 00:00:00
+         */
+        Calendar calendar = Calendar.getInstance();
+        // 定义日期格式
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        System.out.println("Current exec time is : " + simpleDateFormat.format(calendar.getTime()));
+
+        calendar.add(Calendar.SECOND,3);
+
+        // schedule的四种用法
+        /**
+         * 1.在时间等于或超过time的时候执行仅且执行一次task
+         *   如在2016-11-11 00:00:00执行一次task，打印任务名字
+         */
+        //myTimerTask.setName("schedule1");
+        //timer.schedule(myTimerTask,calendar.getTime());
+
+        /**
+         * 2.时间等于或超过time首次执行task
+         *   之后每隔period毫秒重复执行一次task
+         *   如在2016-11-11 00:00:00第一次执行task，打印任务名字
+         *   之后每隔两秒执行一次task
+         */
+        //myTimerTask.setName("schedule2");
+        //timer.schedule(myTimerTask,calendar.getTime(),2000L);
+
+        /**
+         * 3.等待delay毫秒后仅执行且执行一个task
+         *   如现在是2016-11-11 00:00:00
+         *   则在2016-11-11 00:00:01执行一次task，打印任务名字
+         */
+        //myTimerTask.setName("schedule3");
+        //timer.schedule(myTimerTask,1000L);
+
+        /**
+         * 4.等到delay毫秒后首次执行task
+         *   之后每隔period毫秒重复执行一次task
+         *   如现在是2016-11-11 00:00:00
+         *   则在2016-11-11 00:00:01第一次执行task，打印任务名字
+         *   之后每隔两秒执行一次task
+         */
+        //myTimerTask.setName("schedule4");
+        //timer.schedule(myTimerTask,1000L,2000L);
+
+        // scheduleAtFixedRate的两种用法
+        /**
+         * 1.时间等于或超过time时首次执行task
+         *   之后每隔period毫秒重复执行一次task
+         *   如在2016-11-11 00:00:00第一次执行task，打印任务名字
+         *   之后每隔两秒执行一次task
+         */
+        //myTimerTask.setName("scheduleAtFixedRate1");
+        //timer.scheduleAtFixedRate(myTimerTask,calendar.getTime(),2000L);
+
+        /**
+         * 2.等待delay毫秒后首次执行task
+         *   之后每隔period毫秒重复执行一次task
+         *   如现在是2016-11-11 00:00:00
+         *   则在2016-11-11 00:00:01第一次执行task，打印任务名字
+         *   之后每隔两秒执行一次task
+         */
+        myTimerTask.setName("scheduleAtFixedRate2");
+        timer.scheduleAtFixedRate(myTimerTask,1000L,2000L);
     }
 }
--- java-api/src/main/java/com/skilly/timer/MyTimerTask.java
+++ java-api/src/main/java/com/skilly/timer/MyTimerTask.java
@@ -1,18 +1,17 @@
 package com.skilly.timer;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.TimerTask;
 
 /**
  * Created by ${1254109699@qq.com} on 2018/1/15.
- */
-/**
  * 需定时调度的业务逻辑类
  * Created by ChangComputer on 2017/5/24.
  */
-// 继承 TimerTask 类
 public class MyTimerTask extends TimerTask{
     private String name;
+    private int count = 3;
 
     public MyTimerTask(String name){
         this.name = name;
@@ -21,9 +20,22 @@ public class MyTimerTask extends TimerTask{
     // 重写 run 方法
     @Override
     public void run() {
+        if (count < 3) {
+            // 以yyyy-MM-dd HH:mm:ss的格式打印当前执行时间
+            // 如2016-11-11 00:00:00
+            Calendar calendar = Calendar.getInstance();
+            // 定义日期格式
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            System.out.println("Current exec time is : " + simpleDateFormat.format(calendar.getTime()));
 
-        // 打印当前 name 的内容
-        System.out.println("Current exec name is : " + name);
+            // 打印当前 name 的内容
+            System.out.println("Current exec name is : " + name);
+            count--;
+        } else {
+
+            System.out.println("cancel");
+            cancel();
+        }
 
     }
 
