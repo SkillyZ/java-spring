ad8f47ab019ba501ff689a42306424f19c61eb37
周闽强 <zhouminqiang@kuainiugroup.com>
2018-01-16 15:30:04 +0800

    quartz

--- java-api/src/main/java/com/skilly/quartz/HelloJob.java
+++ java-api/src/main/java/com/skilly/quartz/HelloJob.java
@@ -3,24 +3,81 @@ package com.skilly.quartz;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import org.quartz.Job;
-import org.quartz.JobExecutionContext;
-import org.quartz.JobExecutionException;
+import org.quartz.*;
 
-public class HelloJob implements Job {
+public class HelloJob implements Job{
 
+	// 方式二：getter和setter获取
+	// 成员变量 与 传入参数的key一致
+	private String message;
+	private Float floatJobValue;
+	private Double doubleTriggerValue;
+
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public Float getFloatJobValue() {
+		return floatJobValue;
+	}
+	public void setFloatJobValue(Float floatJobValue) {
+		this.floatJobValue = floatJobValue;
+	}
+	public Double getDoubleTriggerValue() {
+		return doubleTriggerValue;
+	}
+	public void setDoubleTriggerValue(Double doubleTriggerValue) {
+		this.doubleTriggerValue = doubleTriggerValue;
+	}
+	@Override
 	public void execute(JobExecutionContext context) throws JobExecutionException {
-		try {
-			Thread.sleep(5000L);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 		// 打印当前的执行时间，格式为2017-01-01 00:00:00
 		Date date = new Date();
 		SimpleDateFormat sf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 		System.out.println("Current Exec Time Is : " + sf.format(date));
-		System.out.println("Hello World");
+
+		// 编写具体的业务逻辑
+		//System.out.println("Hello World!");
+
+		JobKey key = context.getJobDetail().getKey();
+		System.out.println("My name and group are : " + key.getName() + " : " + key.getGroup());
+
+		TriggerKey trkey = context.getTrigger().getKey();
+		System.out.println("My Trigger name and group are : " + trkey.getName() + " : " + trkey.getGroup());
+
+		// 方式一：Map中直接  获取自定义参数
+		JobDataMap jdataMap = context.getJobDetail().getJobDataMap();
+		JobDataMap tdataMap = context.getTrigger().getJobDataMap();
+		String jobMsg = jdataMap.getString("message");
+		Float jobFloatValue = jdataMap.getFloat("floatJobValue");
+
+		String triMsg = tdataMap.getString("message");
+		Double triDoubleValue = tdataMap.getDouble("doubleTriggerValue");
+
+		System.out.println("jobMsg is : " + jobMsg);
+		System.out.println("jobFloatValue is : " + jobFloatValue);
+		System.out.println("triMsg is : " + triMsg);
+		System.out.println("triDoubleValue is : " + triDoubleValue);
+
+		// 方式一：Map中直接获取 获取自定义参数
+		JobDataMap jobDataMap = context.getMergedJobDataMap();
+		jobMsg = jobDataMap.getString("message");
+		jobFloatValue = jobDataMap.getFloat("floatJobValue");
+
+		triMsg = jobDataMap.getString("message");
+		triDoubleValue = jobDataMap.getDouble("doubleTriggerValue");
+
+		System.out.println("jobMsg is : " + jobMsg);
+		System.out.println("jobFloatValue is : " + jobFloatValue);
+		System.out.println("triMsg is : " + triMsg);
+		System.out.println("triDoubleValue is : " + triDoubleValue);
+
+		// 方式二：getter和setter获取
+		System.out.println("message is : " + this.message);
+		System.out.println("jobFloatValue is : " + this.floatJobValue);
+		System.out.println("triDoubleValue is : " + this.doubleTriggerValue);
 	}
 
 }
--- java-api/src/main/java/com/skilly/quartz/HelloScheduler.java
+++ java-api/src/main/java/com/skilly/quartz/HelloScheduler.java
@@ -3,44 +3,50 @@ package com.skilly.quartz;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import org.quartz.CronScheduleBuilder;
-import org.quartz.CronTrigger;
-import org.quartz.JobBuilder;
-import org.quartz.JobDetail;
-import org.quartz.Scheduler;
-import org.quartz.SchedulerException;
-import org.quartz.SchedulerFactory;
-import org.quartz.TriggerBuilder;
+import org.quartz.*;
 import org.quartz.impl.StdSchedulerFactory;
 
 public class HelloScheduler {
 	public static void main(String[] args) throws SchedulerException, InterruptedException {
-		// 打印当前的时间，格式为2017-01-01 00:00:00
-		Date date = new Date();
-		SimpleDateFormat sf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-		System.out.println("Current Time Is : " + sf.format(date));
+
 		// 创建一个JobDetail实例，将该实例与HelloJob Class绑定
 		JobDetail jobDetail = JobBuilder
 				.newJob(HelloJob.class)
-				.withIdentity("myJob")
+				.withIdentity("myjob", "jobgroup1")// 定义标识符
+				.usingJobData("message", "hello myjob1")// 传入自定义参数
+				.usingJobData("floatJobValue", 3.14F)// 传入自定义参数
 				.build();
+
+		System.out.println("jobDetail's name : " + jobDetail.getKey().getName());
+		System.out.println("jobDetail's group : " + jobDetail.getKey().getGroup());
+		System.out.println("jobDetail's jobClass : " + jobDetail.getJobClass().getName());
+
 		CronTrigger trigger = (CronTrigger) TriggerBuilder
 				.newTrigger()
-				.withIdentity("myTrigger", "group1")
+				.withIdentity("myTrigger","trigroup1")// 定义标识符
+				.usingJobData("message", "hello mytrigger1")// 传入自定义参数
+				.usingJobData("doubleTriggerValue", 2.0D)// 传入自定义参数
+//				.startNow()// 定义立即执行
+//				.withSchedule(SimpleScheduleBuilder
+//						.simpleSchedule()
+//						.withIntervalInSeconds(2)
+//						.repeatForever())// 定义执行频度
 				.withSchedule(
 						CronScheduleBuilder.cronSchedule("* * * * * ?"))
 				.build();
-		// 创建Scheduler实例
+		// 创建 Scheduler 实例
 		SchedulerFactory sfact = new StdSchedulerFactory();
 		Scheduler scheduler = sfact.getScheduler();
+
+		// 绑定 JobDetail 和 trigger
+		scheduler.scheduleJob(jobDetail, trigger);
+
+		// 执行任务
 		scheduler.start();
-		System.out.println("scheduled time is :"
-				+ sf.format(scheduler.scheduleJob(jobDetail, trigger)));
-		//scheduler执行两秒后挂起
-		Thread.sleep(2000L);		
-		//shutdown(true)表示等待所有正在执行的job执行完毕之后，再关闭scheduler
-		//shutdown(false)即shutdown()表示直接关闭scheduler
-		scheduler.shutdown(false);
-		System.out.println("scheduler is shut down? " + scheduler.isShutdown());
+
+		// 打印当前的执行时间，格式为2017-01-01 00:00:00
+		Date date = new Date();
+		SimpleDateFormat sf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		System.out.println("Current Time Is : " + sf.format(date));
 	}
 }
