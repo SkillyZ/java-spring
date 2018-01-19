b483fd2380da2eb76a692e271f146be461fcbad3
周闽强 <zhouminqiang@kuainiugroup.com>
2018-01-19 16:35:41 +0800

    spring boot filter bean and tomcat to jetty and change port

---
+++ house/src/main/java/com/skilly/house/filter/LogFilter.java
@@ -0,0 +1,31 @@
+package com.skilly.house.filter;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+/**
+ * Created by ${1254109699@qq.com} on 2018/1/19.
+ */
+public class LogFilter implements Filter {
+    private Logger logger = LoggerFactory.getLogger(LogFilter.class);
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        logger.info("Request coming");
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
