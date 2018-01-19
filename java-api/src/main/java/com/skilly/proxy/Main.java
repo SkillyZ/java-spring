992369bd773a448408ba300358c7b7c5e19a5cb7
周闽强 <zhouminqiang@kuainiugroup.com>
2018-01-19 14:56:20 +0800

    proxy daynamic commit

---
+++ java-api/src/main/java/com/skilly/proxy/Main.java
@@ -0,0 +1,49 @@
+package com.skilly.proxy;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+/**
+ * Created by ${1254109699@qq.com} on 2018/1/19.
+ */
+public class Main {
+    static void customer(ProxyDynamicInterface pi){
+        pi.say();
+    }
+    public static void main(String[] args){
+        RealDynamicObject real = new RealDynamicObject();
+        ProxyDynamicInterface proxy = (ProxyDynamicInterface) Proxy.newProxyInstance(
+                                    ProxyDynamicInterface.class.getClassLoader(),
+                                    new Class[]{ProxyDynamicInterface.class},
+                                    new ProxyDynamicObject(real));
+        customer(proxy);
+    }
+}
+
+
+interface ProxyDynamicInterface{
+    void say();
+}
+
+//被代理类
+class RealDynamicObject implements ProxyDynamicInterface{
+    public void say(){
+        System.out.println("i'm talking");
+    }
+}
+
+//代理类，实现InvocationHandler 接口
+class ProxyDynamicObject implements InvocationHandler {
+    private Object proxied = null;
+    public ProxyDynamicObject(){
+
+    }
+    public ProxyDynamicObject(Object proxied){
+        this.proxied  = proxied;
+    }
+    public Object invoke(Object arg0, Method arg1, Object[] arg2) throws Throwable {
+        System.out.println("hello");
+        return arg1.invoke(proxied, arg2);
+    };
+}
---
+++ java-api/src/main/java/com/skilly/proxy/Main2.java
@@ -0,0 +1,47 @@
+package com.skilly.proxy;
+
+/**
+ * Created by ${1254109699@qq.com} on 2018/1/19.
+ */
+public class Main2 {
+    //这里传入的是接口类型的对象，方便向上转型，实现多态
+    public static void consumer(ProxyInterface pi){
+        pi.say();
+    }
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        consumer(new ProxyObject());
+    }
+}
+
+//代理接口
+interface ProxyInterface{
+    public void say();
+}
+
+
+//被代理者
+class RealObject implements ProxyInterface{
+    //实现接口方法
+    @Override
+    public void say() {
+        // TODO Auto-generated method stub
+        System.out.println("say");
+    }
+
+}
+
+
+//代理者
+class ProxyObject implements ProxyInterface{
+
+    @Override
+    public void say() {
+        // TODO Auto-generated method stub
+        //dosomething for example
+        System.out.println("hello proxy");
+        new RealObject().say();
+        System.out.println("this is method end");
+    }
+
+}
