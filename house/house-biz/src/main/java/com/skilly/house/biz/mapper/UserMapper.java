161e4a1bf89d71a13f69e4a7a262055bf509536e
周闽强 <zhouminqiang@kuainiugroup.com>
2018-01-23 20:10:35 +0800

    add send mail

--- house/house-biz/src/main/java/com/skilly/house/biz/mapper/UserMapper.java
+++ house/house-biz/src/main/java/com/skilly/house/biz/mapper/UserMapper.java
@@ -11,8 +11,14 @@ import java.util.List;
 @Mapper
 public interface UserMapper {
 
-    public List<User> selectUsers();
-
+    public List<User>  selectUsers();
 
     public int insert(User account);
+
+    public int delete(String email);
+
+    public int update(User updateUser);
+
+    public List<User> selectUsersByQuery(User user);
 }
+
