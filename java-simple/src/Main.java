8f88f97e1ed8ce45a9cba2e75381bd67ac5e1dca
skilly <skilly@user1deMac-mini.local>
2018-05-31 10:12:41 +0800

    test new api

--- java-simple/src/Main.java
+++ java-simple/src/Main.java
@@ -1,5 +1,15 @@
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by ${skilly} on 2018/2/24.
@@ -7,10 +17,10 @@ import java.math.RoundingMode;
 public class Main {
 
     public static void main(String[] args) {
-        com();
+
     }
 
-    public static void com()
+    public static void testOptional()
     {
 
         BigDecimal total = new BigDecimal(21);
@@ -22,7 +32,7 @@ public class Main {
 
     }
 
-    public static void com2()
+    public static void testNum()
     {
         Long ll = 1L;
         Long lll = 1213123L;
@@ -32,5 +42,32 @@ public class Main {
         System.out.println(tt);
         Long ttt = Math.round(tt);
         System.out.println(ttt);
+    }
+
+    public static void testInstant(){
+
+        DateTimeFormatter formatter =
+                DateTimeFormatter.ofLocalizedDateTime( FormatStyle.SHORT )
+                        .withLocale( Locale.UK )
+                        .withZone( ZoneId.systemDefault() );
+
+        Instant instant = Instant.now();
+        String output = formatter.format( instant );
+        System.out.println("formatter: " + formatter + " with zone: " + formatter.getZone() + " and Locale: " + formatter.getLocale() );
+        System.out.println("instant: " + instant );
+        System.out.println("output: " + output );
+    }
+
+    public static void testRegx()
+    {
+        List<String> key = new ArrayList<>();
+
+        Pattern pattern = Pattern.compile("\\$\\{(.*?)\\}");
+        Matcher matcher = pattern.matcher("您的验证码是${code}，有效期为${hour}小时，请尽快验证");
+        while (matcher.find())
+        {
+            System.out.println(matcher.group(1));
+            key.add(matcher.group(1));
+        }
     }
 }
