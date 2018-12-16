ab87e62233f68e8a9cf3535cc7fe6e11b02c341e
zhouminqiang <1254109699@qq.com>
2018-12-16 22:01:14 +0800

    hadoop远程任务提交 下载测试完成

--- skilly-hadoop/src/main/java/com/skilly/bigdata/WordCountApp.java
+++ skilly-hadoop/src/main/java/com/skilly/bigdata/WordCountApp.java
@@ -5,14 +5,12 @@ package com.skilly.bigdata;
  */
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
-import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
-import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 import java.io.IOException;
 import java.util.StringTokenizer;
@@ -50,19 +48,34 @@ public class WordCountApp {
     }
 
     public static void main(String[] args) throws Exception {
+        download();
+//        Configuration conf = new Configuration();
+//        Job job = Job.getInstance(conf, "word count");
+//        job.setJarByClass(WordCount.class);
+//        job.setMapperClass(TokenizerMapper.class);
+//        job.setCombinerClass(IntSumReducer.class);
+//        job.setReducerClass(IntSumReducer.class);
+//        job.setOutputKeyClass(Text.class);
+//        job.setOutputValueClass(IntWritable.class);
+//
+//        job.setJar("F:\\javaProject\\java-spring\\skilly-hadoop\\target\\skilly-hadoop-1.0-SNAPSHOT.jar");
+//
+//        FileInputFormat.addInputPath(job, new Path(args[0]));
+//        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+//        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+
+    // 下载文件
+    public static void download() throws IOException
+    {
+        //以下两行用来指明登陆hadoop的用户和你本地的hadoop-2.6.0所存的目录。
+        System.setProperty("HADOOP_USER_NAME", "root");
+        System.setProperty("hadoop.home.dir", "E:\\BigData\\hadoop-3.1.1");
+
         Configuration conf = new Configuration();
-        Job job = Job.getInstance(conf, "word count");
-        job.setJarByClass(WordCount.class);
-        job.setMapperClass(TokenizerMapper.class);
-        job.setCombinerClass(IntSumReducer.class);
-        job.setReducerClass(IntSumReducer.class);
-        job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(IntWritable.class);
-
-        job.setJar("E:\\idea2017workspace\\myhadoop\\out\\artifacts\\myhadoop_jar\\myhadoop.jar");
-
-        FileInputFormat.addInputPath(job, new Path(args[0]));
-        FileOutputFormat.setOutputPath(job, new Path(args[1]));
-        System.exit(job.waitForCompletion(true) ? 0 : 1);
+        FileSystem fs = FileSystem.get(conf);
+        Path src = new Path("E:\\");  // 目标文件
+        Path path = new Path("hdfs://hadoop-master:9000/test/test.txt"); //源文件
+        fs.copyToLocalFile(path,src);
     }
 }
