b9ba322cd812dfc8a5fa665e0187fd18e8d254ee
SkillyZ <1254109699@qq.com>
2018-12-19 20:54:47 +0800

    itemcf 协同过滤 生成评分矩阵

---
+++ skilly-hadoop/src/main/java/com/skilly/bigdata/itemcf/job/JobRunner.java
@@ -0,0 +1,61 @@
+package com.skilly.bigdata.itemcf.job;
+
+import com.skilly.bigdata.itemcf.step1.MR1;
+import com.skilly.bigdata.itemcf.step2.MR2;
+import com.skilly.bigdata.itemcf.step3.MR3;
+import com.skilly.bigdata.itemcf.step4.MR4;
+import com.skilly.bigdata.itemcf.step5.MR5;
+
+/**
+ * @author 周闽强 1254109699@qq.com
+ * @version 1.0.0
+ * @since 18/12/19 下午8:45
+ */
+
+public class JobRunner {
+
+    public static void main(String[] args) {
+        int status1 = -1;
+        int status2 = -1;
+        int status3 = -1;
+        int status4 = -1;
+        int status5 = -1;
+
+        status1 = new MR1().run();
+        if (status1 == 1) {
+            System.out.println("step1运行成功 ，开始运行step2");
+            status2 = new MR2().run();
+        } else {
+            System.out.println("step1运行失败");
+        }
+
+        if (status2 == 1) {
+            System.out.println("step2运行成功 ，开始运行step3");
+            status3 = new MR3().run();
+        } else {
+            System.out.println("step2运行失败");
+        }
+
+        if (status3 == 1) {
+            System.out.println("step3运行成功 ，开始运行step4");
+            status4 = new MR4().run();
+        } else {
+            System.out.println("step3运行失败");
+        }
+
+        if (status4 == 1) {
+            System.out.println("step4运行成功 ，开始运行step5");
+            status5 = new MR5().run();
+        } else {
+            System.out.println("step1运行失败");
+        }
+
+        if (status5 == 1) {
+            System.out.println("step5运行成功 ，程序结束");
+        } else {
+            System.out.println("step5运行失败");
+        }
+
+    }
+
+}
