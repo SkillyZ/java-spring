690f3105a1ca957acb1f214f147ce373c012fd2a
SkillyZ <1254109699@qq.com>
2018-12-25 20:09:21 +0800

    spark thrift jdbc操作

---
+++ skilly-spark/src/main/scala/com/skilly/spark/SparkSQLThriftServerApp.scala
@@ -0,0 +1,26 @@
+package com.skilly.spark
+
+import java.sql.DriverManager
+
+
+/**
+  * 通过jdbc 方式
+  */
+object SparkSQLThriftServerApp {
+    def main(args: Array[String]): Unit = {
+        Class.forName("org.apache.hive.jdbc.HiveDriver");
+
+        val conn = DriverManager.getConnection("jdbc:hive2://198.13.33.218:10000", "hadoop", "");
+        val pstmt = conn.prepareStatement("select empno, ename, sal from emp");
+        val rs = pstmt.executeQuery();
+        while (rs.next()) {
+            println("empno :" + rs.getInt("empno")
+            + "ename :" + rs.getString("ename")
+            + "sal :" + rs.getDouble("sal") );
+        }
+
+        rs.close();
+        pstmt.close();
+        conn.close();
+    }
+}
