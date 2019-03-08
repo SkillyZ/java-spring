4b7b5116328e58d34cb3296fe3ba66251fafb026
SkillyZ <1254109699@qq.com>
2019-03-08 21:01:44 +0800

    depend upgrade

--- sericefeign/src/main/java/com/skilly/feign/ServiceFeignApplication.java
+++ sericefeign/src/main/java/com/skilly/feign/ServiceFeignApplication.java
@@ -3,7 +3,7 @@ package com.skilly.feign;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * Created by 1254109699@qq.com on 2018/1/8.
