f965358da483cff15ef2aaa7b05a4b850d892c6d
SkillyZ <1254109699@qq.com>
2019-03-21 19:11:47 +0800

    add algorithm

---
+++ java-algorithm/src/main/java/algorithm/Graph.java
@@ -0,0 +1,288 @@
+package algorithm;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Graph {
+
+    private List<Vertex> vertexList;
+    private Map<Vertex, List<Edge>> verEdgeListMap;
+
+    public Graph(List<Vertex> vertexList, Map<Vertex, List<Edge>> verEdgeListMap) {
+        super();
+        this.vertexList = vertexList;
+        this.verEdgeListMap = verEdgeListMap;
+    }
+
+    public List<Vertex> getVertexList() {
+        return vertexList;
+    }
+
+    public void setVertexList(List<Vertex> vertexList) {
+        this.vertexList = vertexList;
+    }
+
+
+    public Map<Vertex, List<Edge>> getVerEdgelistMap() {
+        return verEdgeListMap;
+    }
+
+    public void setVerEdgelistMap(Map<Vertex, List<Edge>> verEdgeListMap) {
+        this.verEdgeListMap = verEdgeListMap;
+    }
+
+
+    static class Edge {
+        private Vertex startVertex;
+        private Vertex endVertex;
+        private int weight;
+
+        public Edge(Vertex startVertex, Vertex endVertex, int weight) {
+            super();
+            this.startVertex = startVertex;
+            this.endVertex = endVertex;
+            this.weight = weight;
+        }
+
+        public Edge() {
+        }
+
+        public Vertex getStartVertex() {
+            return startVertex;
+        }
+
+        public void setStartVertex(Vertex startVertex) {
+            this.startVertex = startVertex;
+        }
+
+        public Vertex getEndVertex() {
+            return endVertex;
+        }
+
+        public void setEndVertex(Vertex endVertex) {
+            this.endVertex = endVertex;
+        }
+
+        public int getWeight() {
+            return weight;
+        }
+
+        public void setWeight(int weight) {
+            this.weight = weight;
+        }
+    }
+
+    static class Vertex {
+        private final static int infinite_dis = Integer.MAX_VALUE;
+
+        /**
+         * node name
+         */
+        private String name;
+        /**
+         * before node know
+         */
+        private boolean known;
+        /**
+         *  none distance
+         */
+        private int adjuDist;
+        /**
+         *  Shortest path of parent node
+         */
+        private Vertex parent;
+
+        public Vertex() {
+            this.known = false;
+            this.adjuDist = infinite_dis;
+            this.parent = null;
+        }
+
+        public Vertex(String name) {
+            this.known = false;
+            this.adjuDist = infinite_dis;
+            this.parent = null;
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public boolean isKnown() {
+            return known;
+        }
+
+        public void setKnown(boolean known) {
+            this.known = known;
+        }
+
+        public int getAdjuDist() {
+            return adjuDist;
+        }
+
+        public void setAdjuDist(int adjuDist) {
+            this.adjuDist = adjuDist;
+        }
+
+        public Vertex getParent() {
+            return parent;
+        }
+
+        public void setParent(Vertex parent) {
+            this.parent = parent;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (!(obj instanceof Vertex)) {
+                throw new ClassCastException("an object to compare with a Vertext must be Vertex");
+            }
+            if (this.name == null) {
+                throw new NullPointerException("name of Vertex to be compared cannot be null");
+            }
+            return this.name.equals(((Vertex) obj).name);
+        }
+    }
+
+    public void setRoot(Vertex v) {
+        v.setParent(null);
+        v.setAdjuDist(0);
+    }
+
+
+    /**
+     * search shortest Length
+     * @param startIndex dijkstra start index
+     * @param destIndex  dijkstra end index
+     */
+    public void dijkstraTravasal(int startIndex, int destIndex) {
+        Vertex start = vertexList.get(startIndex);
+        Vertex destEnd = vertexList.get(destIndex);
+        Vertex dest = vertexList.get(destIndex);
+        String path = "[" + dest.getName() + "]";
+
+        setRoot(start);
+        updateChildren(vertexList.get(startIndex));
+
+        int shortestLength = dest.getAdjuDist();
+
+        while ((dest.getParent() != null) && (!dest.equals(start))  ) {
+            path = "[" + dest.getParent().getName() + "] --> " + path;
+            dest = dest.getParent();
+            if (dest.equals(destEnd)) {
+                break;
+            }
+        }
+
+        System.out.println("[" + vertexList.get(startIndex).getName() + "] to [" + vertexList.get(destIndex).getName() + "] dijkstra shortest path :: " + path);
+        System.out.println("shortest length::" + shortestLength);
+    }
+
+    public void dijkstraTravasal(List<Integer> destIndex) {
+        if (destIndex.size() == 0) {
+            return ;
+        }
+        for (int i = 0; (i + 1) < destIndex.size(); i++) {
+            int startIndex = destIndex.get(i);
+            int nextIndex = destIndex.get(i + 1);
+            dijkstraTravasal(startIndex, nextIndex);
+        }
+    }
+
+    /**
+     * straight line match
+     * @param destIndex
+     */
+    public void straightTravasal(List<Integer> destIndex) {
+        if (destIndex.size() == 0) {
+            return ;
+        }
+        int dist = 0;
+        for (int i = 0; (i + 1) < destIndex.size(); i++) {
+            int startIndex = destIndex.get(i);
+            int nextIndex = destIndex.get(i + 1);
+
+            Vertex start = vertexList.get(startIndex);
+            Vertex dest = vertexList.get(nextIndex);
+            List<Edge> list = verEdgeListMap.get(start).stream()
+                    .filter(edge -> edge.getStartVertex().equals(start) && edge.getEndVertex().equals(dest))
+                    .collect(Collectors.toList());
+            if (list.size() == 0) {
+                System.out.println("NO SUCH ROUTE");
+                return;
+            }
+            dist += list.get(0).getWeight();
+        }
+        System.out.println(dist);
+    }
+
+    /**
+     * count line
+     * @param startIndex
+     * @param destIndex
+     */
+    public void countTravasal(int startIndex, int destIndex) {
+
+    }
+
+    /**
+     * max count line
+     * @param startIndex
+     * @param destIndex
+     */
+    public void cmaxCountTravasal(int startIndex, int destIndex) {
+
+    }
+
+    /**
+     * 从初始节点开始递归更新邻接表
+     *
+     *  step1 如果子节点之前未知，则把当前子节点假如更新列表
+     *  step2 更新每一个子节点
+     * @param v
+     */
+    private void updateChildren(Vertex v) {
+        if (v == null) {
+            return;
+        }
+        if (verEdgeListMap.get(v) == null || verEdgeListMap.get(v).size() == 0) {
+            return;
+        }
+
+        List<Vertex> childrenList = new LinkedList<Vertex>();
+        for (Edge e : verEdgeListMap.get(v)) {
+            Vertex childVertex = e.getEndVertex();
+
+            if (!childVertex.isKnown()) {
+                childVertex.setKnown(true);
+                childVertex.setAdjuDist(v.getAdjuDist() + e.getWeight());
+                childVertex.setParent(v);
+                childrenList.add(childVertex);
+            }
+
+            int nowDist = v.getAdjuDist() + e.getWeight();
+            if (nowDist >= childVertex.getAdjuDist()) {
+                continue;
+            } else {
+                childVertex.setAdjuDist(nowDist);
+                childVertex.setParent(v);
+            }
+        }
+
+        for (Vertex vc : childrenList) {
+            updateChildren(vc);
+        }
+    }
+
+
+    private void clear() {
+
+    }
+}
---
+++ java-algorithm/src/main/java/algorithm/TestGraph.java
@@ -0,0 +1,87 @@
+package algorithm;
+
+import java.util.*;
+
+import algorithm.Graph.*;
+
+/**
+ * 测试用main方法
+ *
+ * @author wuhui.wwh
+ */
+public class TestGraph {
+    public static void main(String[] args) {
+        Vertex A = new Vertex("A");
+        Vertex B = new Vertex("B");
+        Vertex C = new Vertex("C");
+        Vertex D = new Vertex("D");
+        Vertex E = new Vertex("E");
+
+        List<Vertex> verList = new LinkedList<Vertex>();
+        verList.add(A);
+        verList.add(B);
+        verList.add(C);
+        verList.add(D);
+        verList.add(E);
+
+        Map<Vertex, List<Edge>> vertexEdgeListMap = new HashMap<Vertex, List<Edge>>();
+
+        List<Edge> AList = new LinkedList<Edge>();
+        AList.add(new Edge(A, B, 5));
+        AList.add(new Edge(A, D, 5));
+        AList.add(new Edge(A, E, 7));
+
+
+        List<Edge> BList = new LinkedList<Edge>();
+        BList.add(new Edge(B, C, 4));
+
+        List<Edge> CList = new LinkedList<Edge>();
+        CList.add(new Edge(C, D, 8));
+        CList.add(new Edge(C, E, 2));
+
+        List<Edge> DList = new LinkedList<Edge>();
+        DList.add(new Edge(D, C, 8));
+        DList.add(new Edge(D, E, 6));
+
+        List<Edge> EList = new LinkedList<Edge>();
+        EList.add(new Edge(E, B, 3));
+
+        vertexEdgeListMap.put(A, AList);
+        vertexEdgeListMap.put(B, BList);
+        vertexEdgeListMap.put(C, CList);
+        vertexEdgeListMap.put(D, DList);
+        vertexEdgeListMap.put(E, EList);
+
+
+        List<Integer> list = new ArrayList<Integer>();
+        Graph g = new Graph(verList, vertexEdgeListMap);
+
+        g.straightTravasal(Arrays.asList(0,1,2));
+        g.straightTravasal(Arrays.asList(0,3));
+        g.straightTravasal(Arrays.asList(0,3,2));
+        g.straightTravasal(Arrays.asList(0,4,1,2,3));
+        g.straightTravasal(Arrays.asList(0,4,3));
+
+        g.dijkstraTravasal(0,2);
+        g.dijkstraTravasal(1,1);
+        g.dijkstraTravasal(1,1);
+//        list.add(0);
+//        list.add(3);
+//        list.add(2);
+//        g.dijkstraTravasal(list);
+//        list.clear();
+//        list.add(0);
+//        list.add(4);
+//        list.add(1);
+//        list.add(2);
+//        list.add(3);
+//        //A-E-B-C-D
+//        g.dijkstraTravasal(list);
+//
+//        list.clear();
+//        list.add(0);
+//        list.add(4);
+//        list.add(3);
+//        g.dijkstraTravasal(list);
+    }
+}
